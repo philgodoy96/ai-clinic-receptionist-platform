b8b47ead1bf05f2f0afbce457cc183d50ad8a8e8
﻿Felipe Godoy <godoyphil9@gmail.com>
2026-06-19 04:24:45 -0300

    fix: clean scheduling model imports

--- app/models/scheduling.py
+++ app/models/scheduling.py
@@ -8,7 +8,6 @@ from sqlalchemy import (
     CheckConstraint,
     Date,
     DateTime,
-    Enum as SAEnum,
     ForeignKey,
     Index,
     String,
@@ -16,6 +15,9 @@ from sqlalchemy import (
     UniqueConstraint,
     Uuid,
     func,
+)
+from sqlalchemy import (
+    Enum as SAEnum,
 )
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
